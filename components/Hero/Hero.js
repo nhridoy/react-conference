ee73a6740946d5c30666b9e11d79d784738cbafd
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-11-23 23:31:53 +0600

    hero
    Signed-off-by: Nahidujjaman Hridoy <hridoyboss12@gmail.com>

---
+++ components/Hero/Hero.js
@@ -0,0 +1,44 @@
+import { BsArrowUpRight } from "react-icons/bs";
+import { FiArrowLeft } from "react-icons/fi";
+import Image from "next/image";
+const Hero = () => {
+  return (
+    <div className="px-6 grid md:grid-cols-12 gap-5 relative">
+      <h1 className="text-5xl md:text-8xl font-bold text-right md:col-start-3 md:col-span-5 text-clr order-1">
+        React Conference
+      </h1>
+
+      <Image
+        src="/images/banner_2.png"
+        width={1000}
+        height={1000}
+        className="md:col-start-1 md:col-span-4 w-1/2 md:w-96 order-4 md:order-2"
+      />
+
+      <div className="md:col-start-5 md:col-end-8 md:col-span-1 flex flex-col gap-5 order-2 md:order-3">
+        <p className="text-justify">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          veniam ipsa, natus quisquam architecto optio, reiciendis recusandae
+          culpa, iusto necessita
+        </p>
+        <div className="">
+          <button className="bg-[#FFC93E] px-6 md:px-14 py-3 rounded-full font-semibold flex items-center gap-4 mx-auto md:mx-0 text-sm">
+            Buy Tickets <BsArrowUpRight />
+          </button>
+        </div>
+      </div>
+
+      <Image
+        src="/images/banner_1.png"
+        width={10000}
+        height={1000}
+        className="md:col-start-8 md:col-end-12 w-full md:w-96 md:absolute md:top-32 md:ml-5 order-3 md:order-4"
+      />
+      <p className="hidden cursor-pointer order-5 col-start-6 -rotate-90 md:absolute bottom-32 md:flex items-center gap-4">
+        <FiArrowLeft /> Scroll Down
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
